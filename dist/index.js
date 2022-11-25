3c66c5586b70a2d107af57360d125f52e3d466a9
shah <workwithme.shah@gmail.com>
2022-11-25 12:54:57 +0530

    array tuple

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,18 @@
 "use strict";
-let age = 24;
-if (age >= 18) {
-    console.log("Eligible for voting");
-}
+let day1 = "monday";
+let day2 = "tuesday";
+let day3 = "wednesday";
+let sale = 45789;
+let num;
+num = 1;
+num = "what??";
+let isPublished = true;
+let numb = [1, 2, 3, 4];
+let numb1 = [];
+numb1[0] = 5;
+numb1[1] = 25;
+numb1[2] = 15;
+numb1[3] = 65;
+let user = [5, "Shah"];
+user.push(5);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,26 @@
-let age: number = 24;
+let day1 = "monday";
+let day2 = "tuesday";
+let day3 = "wednesday";
+let sale = 45_789;
 
-if (age >= 18) {
-    console.log("Eligible for voting");
-}
+//because it is of type any. 
+let num;
+num = 1;
+num = "what??"
+
+
+let isPublished: boolean = true;
+// #Array
+let numb: number[] = [1, 2, 3, 4];
+
+// for empty array
+let numb1: number[] = [];
+
+numb1[0] = 5;
+numb1[1] = 25;
+numb1[2] = 15;
+numb1[3] = 65;
+
+//Tuples
+let user: [number, string] = [5, "Shah"];
+
