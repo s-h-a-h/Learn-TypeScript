0541284fff8dd6b25ff955f6a61df841a5242289
shah <workwithme.shah@gmail.com>
2022-11-25 12:14:13 +0530

    tsc initialised and setup debugger

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+let age = 24;
+if (age >= 18) {
+    console.log("Eligible for voting");
+}
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+let age: number = 24;
+
+if (age >= 18) {
+    console.log("Eligible for voting");
+}
