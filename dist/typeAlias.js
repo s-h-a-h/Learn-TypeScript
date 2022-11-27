903ac86e1a394ecc9227a48beeaeb6befbe9a64b
Shah <workwithme.shah@gmail.com>
2022-11-27 22:30:12 +0530

    READONLY & optionals

--- dist/typeAlias.js
+++ dist/typeAlias.js
@@ -1,10 +1,24 @@
 "use strict";
 const saveUser = (user) => {
     console.table(user);
+    return user;
 };
 saveUser({
+    _id: 6549846478,
     name: "Shah",
     email: "sha@h.com",
     isActive: true
 });
+let newUser = {
+    _id: 65487987,
+    name: "Shah",
+    email: "sh@h.com",
+    isActive: false,
+};
+newUser.email = "asd@pl.com";
+let userCardDetail = {
+    cardNo: 36235644895,
+    cardDate: "22/10/2020",
+    cvv: 544
+};
 //# sourceMappingURL=typeAlias.js.map
--- src/typeAlias.ts
+++ src/typeAlias.ts
@@ -1,9 +1,11 @@
 // To create own type
 
 type User = {
+    readonly _id: number, //only read no write
     name: string,
     email: string,
-    isActive: boolean
+    isActive: boolean,
+    creditCardNumber?: number //optional
 }
 
 
@@ -13,7 +15,39 @@ const saveUser = (user: User): User => {
 }
 
 saveUser({
+    _id: 6549846478,
     name: "Shah",
     email: "sha@h.com",
     isActive: true
 })
+
+let newUser: User = {
+    _id: 65487987,
+    name: "Shah",
+    email: "sh@h.com",
+    isActive: false,
+}
+
+// newUser._id = 54531355; not allowed
+newUser.email = "asd@pl.com"
+
+
+// mix and match of type
+type cardNumber = {
+    cardNo: number
+}
+
+type cardDate = {
+    cardDate: string
+}
+
+
+type cardDetails = cardNumber & cardDate & {
+    cvv: number
+}
+
+let userCardDetail: cardDetails = {
+    cardNo: 36235644895,
+    cardDate: "22/10/2020",
+    cvv: 544
+}
