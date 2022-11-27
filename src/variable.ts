eae869f29bce07b2c4d36bc7ae50c83bbe9496c3
Shah <workwithme.shah@gmail.com>
2022-11-27 19:06:21 +0530

    no implicit any - tsconfig

---
+++ src/variable.ts
@@ -0,0 +1,7 @@
+let userId: number = 4561531;
+let isLoggedIn: boolean = true;
+let age = 24; //type inferencea is not compulsory
+let done = false; //type inferencea is not compulsory
+// age = "shah";  still not allowed// 
+// done = 24; still not allowed
+console.log(userId.toString);
